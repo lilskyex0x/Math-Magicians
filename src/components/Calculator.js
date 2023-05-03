9be5481a812201673cec877db8e61effb79676e3
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-05-03 22:24:03 +0630

    Changes and add child component in calc component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import '../style.css';
+import CalcBtns from './CalculatorBtn';
 
 function Calculator() {
   return (
@@ -7,37 +8,7 @@ function Calculator() {
         <div className="previous__num">354 +</div>
         <div className="current__num">521,879</div>
       </div>
-      <button type="submit">AC</button>
-      <button type="submit">+/-</button>
-      <button type="submit">%</button>
-      <button type="submit" className="ops__btn">
-        ÷
-      </button>
-      <button type="submit">7</button>
-      <button type="submit">8</button>
-      <button type="submit">9</button>
-      <button type="submit" className="ops__btn">
-        x
-      </button>
-      <button type="submit">4</button>
-      <button type="submit">5</button>
-      <button type="submit">6</button>
-      <button type="submit" className="ops__btn">
-        -
-      </button>
-      <button type="submit">1</button>
-      <button type="submit">2</button>
-      <button type="submit">3</button>
-      <button type="submit" className="ops__btn">
-        +
-      </button>
-      <button type="submit" className="span__two">
-        0
-      </button>
-      <button type="submit">.</button>
-      <button type="submit" className="ops__btn">
-        =
-      </button>
+      <CalcBtns />
     </div>
   );
 }
